a5eab3777836e63396e60b72dcd8e771b819e5b3
Frias <dofriascomurgencia@gmail.com>
2012-08-06 15:17:09 +0100

    Editados os parametros de acesso a base de dados

--- conexao.php
+++ conexao.php
@@ -1,4 +1,4 @@
 ﻿<?php
-	$cn=mysqli_connect("localhost", "admin", "admin","bd2012");
-		if (!$cn) print ("problemas na ligacao ao servidor Mysql");
+	$cn=mysqli_connect("localhost", "admin", "admin","intern");
+		if (!$cn) print ("Problemas na ligacao ao servidor Mysql");
 ?>
